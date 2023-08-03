810805eb3b92a24e7c7fbe25aaa93e56f31546d9
Muhammad Rafly Sadewa <msadewa@pajak.io>
2023-08-03 12:19:47 +0700

    update finish file meta data project

--- index.js
+++ index.js
@@ -1,20 +1,42 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
-var app = express();
+const app = express();
+const multer = require("multer");
 
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
-
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/"); // folder penyimpanan file yang diupload
+  },
+  filename: function (req, file, cb) {
+    // Untuk penamaan file yang diupload untuk di simpan ke folder destination
+    cb(null, file.originalname);
+  },
 });
 
+const upload = multer({ storage: storage });
 
+app.use(cors());
+app.use("/public", express.static(process.cwd() + "/public"));
 
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
+
+app.post("/api/fileanalyse", upload.single("upfile"), function (req, res) {
+  if (!req.file) {
+    return res.status(400).json({ error: "No file uploaded." });
+  }
+
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
